7cfd83788e327cd7175ffc9efa36a7a437368b78
alepaez-dev <al3_hp@hotmail.com>
2021-10-08 12:22:02 -0600

    feat: add font, changes on tailwind config

--- components/tailwind-styles/tailwind.config.js
+++ components/tailwind-styles/tailwind.config.js
@@ -1,21 +1,86 @@
-// tailwind.config.js
+// tailwind config de intercath
 module.exports = {
-  purge: [],
+  mode: 'all',
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    spacing: {
-      16: "1.6rem",
-      20: "2rem",
-    },
     extend: {
-      spacing: {
-        16: "1.6rem",
-        20: "2rem",
+      inset: {
+        76: '0.76rem',
+      },
+      top: {
+        76: '0.76rem',
+      },
+      colors: {
+        primary: '#5D75F9',
+        secondary: '#1D2A48',
+        third: '#9EC6FA',
+        'dark-blue': '#111827',
+        'light-blue': '#9EBCF6',
+      },
+      fontSize: {
+        'extra-small': '0.5rem',
+      },
+      zIndex: {
+        negative: -1,
+      },
+      minWidth: {
+        64: '64px',
+      },
+      maxWidth: {
+        32: '32px',
+        36: '36px',
+        64: '64px',
+        94: '94px',
+        136: '136px',
+      },
+      minHeight: {
+        64: '64px',
+        94: '94px',
+        136: '136px',
+      },
+      maxHeight: {
+        24: '24px',
+        32: '32px',
+        64: '64px',
+        94: '94px',
+        136: '136px',
+      },
+      backgroundColor: {
+        primary: '#5D75F9',
+        secondary: '#1D2A48',
+        third: '#9EC6FA',
+        'dark-blue': '#111827',
+        tag_status_1: '#EFF6FF',
+        tag_status_2: '#CFFAFE',
+        tag_status_3: '#D1FAE5',
+        tag_status_4: '#FEF3C7',
+        tag_status_5: '#FEE2E2',
+        tag_status_6: '#EDE9FE',
+        tag_status_7: '#FCE7F3',
+        tag_status_8: '#E0E7FF',
+        tag_status_9: '#DBEAFE',
+        tag_status_10: '#F3F4F6',
+        blue_purple: '#9EBCF6',
+      },
+      textColor: {
+        tag_status_1: '#3B82F6',
+        tag_status_2: '#155E75',
+        tag_status_3: '#065F46',
+        tag_status_4: '#B45309',
+        tag_status_5: '#991B1B',
+        tag_status_6: '#5B21B6',
+        tag_status_7: '#9D174D',
+        tag_status_8: '#3730A3',
+        tag_status_9: '#1E40AF',
+        tag_status_10: '#1F2937',
+      },
+      fontFamily: {
+        body: ['Montserrat'],
       },
     },
   },
   variants: {
     extend: {},
-  },
-  plugins: [],
+  }
 };
